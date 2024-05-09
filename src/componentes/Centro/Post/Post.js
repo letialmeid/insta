cd872f2d2ce0c5fd493e73697853c781e559bd67
Letícia Nascimento de Almeida <161456572+letialmeid@users.noreply.github.com>
2024-05-09 17:59:33 -0300

    Update Post.js

--- src/componentes/Centro/Post/Post.js
+++ src/componentes/Centro/Post/Post.js
@@ -1,33 +1,40 @@
-import './Post.css'
-
-import { CiHeart } from "react-icons/ci";
-import { FiMessageCircle } from "react-icons/fi";
-import { GoPaperAirplane } from "react-icons/go";
-
+import React, { useState } from 'react';
+import './Post.css';
+import { CiHeart } from 'react-icons/ci';
+import { FiMessageCircle } from 'react-icons/fi';
+import { GoPaperAirplane } from 'react-icons/go';
 
 export default function Post(props) {
-    return (
-        <div className="card Post">
-            <div className="card-header">
-                <img src={props.foto} className='foto'></img>
-                <h4>{props.title}</h4>
-            </div>
-            <div className="card-body">
-                <img src={props.imgSrc} />
-                <div className='icones'>
-                    <CiHeart class="icon" style={{ fontSize: "35px", color:'white'  }} />
-                    <FiMessageCircle style={{ fontSize: "27px" }} />
-                    <GoPaperAirplane style={{ fontSize: "27px" }} />
-                </div>
+  const [liked, setLiked] = useState(false);
 
-                <div className='comentario'>
-                    <h4>{props.titular}</h4>
-                    <p>{props.body}</p>
-                </div>
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
 
-            </div>
-            <div className="card-footer">
-            </div>
+  return (
+    <div className="card Post">
+      <div className="card-header">
+        <img src={props.foto} className="foto" alt="user"></img>
+        <h4>{props.title}</h4>
+      </div>
+      <div className="card-body">
+        <img src={props.imgSrc} alt="post"></img>
+        <div className="icones">
+          <CiHeart
+            className="icon"
+            style={{ fontSize: '35px', color: liked ? 'red' : 'white' }}
+            onClick={handleLikeClick}
+          />
+          <FiMessageCircle style={{ fontSize: '27px' }} />
+          <GoPaperAirplane style={{ fontSize: '27px' }} />
         </div>
-    )
+
+        <div className="comentario">
+          <h4>{props.titular}</h4>
+          <p>{props.body}</p>
+        </div>
+      </div>
+      <div className="card-footer"></div>
+    </div>
+  );
 }
